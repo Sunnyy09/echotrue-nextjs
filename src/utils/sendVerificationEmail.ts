94980829d8276375a85adfb816ef5487b72c0ef6
Sunnyy09 <12001109@csepup.ac.in>
2025-01-29 09:50:19 +0530

    Add functionality of otp through email

---
+++ src/utils/sendVerificationEmail.ts
@@ -0,0 +1,24 @@
+import { resend } from "../lib/resend";
+import VerificationEmail from "../../emails/verificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+import { url } from "inspector";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verifyCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "Mystry Message | Verification Code",
+      react: VerificationEmail({ username, otp: verifyCode }),
+    });
+
+    return { success: true, message: "Verification email send successfully " };
+  } catch (emailError) {
+    console.error("Error during sending verification email", emailError);
+    return { success: false, message: "Failed to send verification email" };
+  }
+}
