230ac14376037694a429df04e7fd008beaa05034
Sunnyy09 <12001109@csepup.ac.in>
2025-01-30 11:02:39 +0530

    Add self declare types and layout testing for sign-in route

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,29 @@
+import "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
+
+declare module "next-auth" {
+  interface User {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
+  interface Session {
+    user: {
+      _id?: string;
+      isVerified?: boolean;
+      isAcceptingMessages?: boolean;
+      username?: string;
+    } & DefaultSession["user"];
+  }
+}
+
+// alternative way to declare
+declare module "next-auth/jwt" {
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessages?: boolean;
+    username?: string;
+  }
+}
