bf11637aecea5fa58a49541b4228ec2690ecbfff
Sunnyy09 <12001109@csepup.ac.in>
2025-01-30 10:58:38 +0530

    Add nextAuth setup with email verification and using zod schemas

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,82 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "Enter your email",
+        },
+        password: { lebel: "Password", type: "password" },
+      },
+      async authorize(credentials: any): Promise<any> {
+        await dbConnect();
+
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.identifier.email },
+              { username: credentials.identifier.username },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Incorrect Password");
+          }
+        } catch (err: any) {
+          throw new Error(err);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.username = user.username;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET_KEY,
+};
---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,81 @@
+import { z } from "zod";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import { verifySchema } from "@/Schemas/verifySchema";
+import { NextResponse } from "next/server";
+
+const verifyCodeSchema = z.object({
+  verifyCode: verifySchema,
+});
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, code } = await request.json();
+
+    const decodedUsername = decodeURIComponent(username);
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "user not found",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+
+      return NextResponse.json(
+        {
+          success: true,
+          message: "Account verified successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return NextResponse.json(
+        {
+          success: false,
+          message:
+            "verification code expired, please sign in again to a new code",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else {
+      return NextResponse.json(
+        {
+          success: false,
+          message: "Incorrect verification code",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+  } catch (error) {
+    console.error("Error verifying user", error);
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Error verifying user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
